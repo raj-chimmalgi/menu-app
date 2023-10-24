5766ad4760eed421a4fa4ce24a16792c078a9db9
Raj Chimmalgi <chimmalgi@mgail.com>
2023-10-23 23:19:13 -0700

    Display menu, categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import Title from "./Title.jsx";
+import data from "./data.js";
+import Menu from "./Menu.jsx";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(data.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(data);
+  const [categories, setCategories] = useState(allCategories);
+
+  return (
+    <main>
+      <section>
+        <Title text="Our Menu" />
+        <Categories categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
---
+++ src/Categories.jsx
@@ -0,0 +1,15 @@
+const Categories = ({ categories }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button className="btn" type="button" key={category}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
